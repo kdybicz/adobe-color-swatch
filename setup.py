6c8a9dd990fb011119e6094644c4e00de303d6ae
Kamil Dybicz <kamil.dybicz@gmail.com>
2022-01-30 13:34:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="adobe-swatch",
+    name="adobe-color-swatch",
     version="1.0.0",
     entry_points={
         "console_scripts": ["swatch=swatch.cli:main"],
@@ -14,5 +14,5 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kdybicz/adobe-swatch-generator",
-    description="The finest selection of Chuck Norris jokes."
+    description="Simple Python script to extract and generate Adobe Color Swatch (.aco) files."
 )
